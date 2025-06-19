154b8d065f965144f73aa03cae71ca708e3d62ba
Avi Jain <avijj1726@gmail.com>
2025-06-19 12:45:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const path = require("path");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.set("views", path.join(__dirname, "views"));
-app.set("views", path.join(__dirname, "public"));
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
