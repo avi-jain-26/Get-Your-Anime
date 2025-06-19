d594bb547e435a425c84d2c5b682f09d24e4f6dd
Avi Jain <avijj1726@gmail.com>
2025-06-19 12:23:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express();
 const port = 8080;
 const path = require("path");
 app.set("views", path.join(__dirname, "views"));
-
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 
 app.listen(port, (req)=>{
   console.log("server started at port localHost:",port);
